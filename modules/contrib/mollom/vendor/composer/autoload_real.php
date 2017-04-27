7eac1029319ca685ec14c8e35ea1e911ee4f6612
Shaun Moss <shaun@astromultimedia.com>
2017-04-27 16:11:22 -0500

    Added mollom vendor files

--- modules/contrib/mollom/vendor/composer/autoload_real.php
+++ modules/contrib/mollom/vendor/composer/autoload_real.php
@@ -23,30 +23,28 @@ class ComposerAutoloaderInitfc0313a8046fdb97b2688271291fa80e
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitfc0313a8046fdb97b2688271291fa80e', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
-        if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+        $map = require __DIR__ . '/autoload_namespaces.php';
+        foreach ($map as $namespace => $path) {
+            $loader->set($namespace, $path);
+        }
 
-            call_user_func(\Composer\Autoload\ComposerStaticInitfc0313a8046fdb97b2688271291fa80e::getInitializer($loader));
-        } else {
-            $map = require __DIR__ . '/autoload_namespaces.php';
-            foreach ($map as $namespace => $path) {
-                $loader->set($namespace, $path);
-            }
+        $map = require __DIR__ . '/autoload_psr4.php';
+        foreach ($map as $namespace => $path) {
+            $loader->setPsr4($namespace, $path);
+        }
 
-            $map = require __DIR__ . '/autoload_psr4.php';
-            foreach ($map as $namespace => $path) {
-                $loader->setPsr4($namespace, $path);
-            }
-
-            $classMap = require __DIR__ . '/autoload_classmap.php';
-            if ($classMap) {
-                $loader->addClassMap($classMap);
-            }
+        $classMap = require __DIR__ . '/autoload_classmap.php';
+        if ($classMap) {
+            $loader->addClassMap($classMap);
         }
 
         $loader->register(true);
 
         return $loader;
     }
+}
+
+function composerRequirefc0313a8046fdb97b2688271291fa80e($file)
+{
+    require $file;
 }
