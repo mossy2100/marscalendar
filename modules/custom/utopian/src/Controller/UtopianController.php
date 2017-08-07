5360799f7ba7942c8b0286a0c7cd3eda453d250e
Shaun Moss <shaun@astromultimedia.com>
2017-08-07 17:14:41 +0100

    Started creating callbacks for generating calendar pages and the converter.

--- modules/custom/utopian/src/Controller/UtopianController.php
+++ modules/custom/utopian/src/Controller/UtopianController.php
@@ -2,7 +2,6 @@
 /**
  * Created by PhpStorm at 2017-08-06T19:51
  * @author Shaun Moss (shaun@astromultimedia.com, @mossy2100)
-
  * @file
  * Contains \Drupal\utopian\Controller\UtopianController.
  */
@@ -14,4 +13,169 @@ namespace Drupal\utopian\Controller;
  */
 class UtopianController {
 
+  /**
+   * Displays the converter.
+   *
+   * @return array
+   *   A render array representing the converter page content.
+   */
+  public function converter() {
+    $build = [
+      '#type'  => 'markup',
+      '#theme' => 'utopian_converter',
+      '#cache' => [
+        'max-age' => 0,
+      ],
+    ];
+    return $build;
+  }
+
+  /**
+   * Displays the calendar pages.
+   *
+   * @return array
+   *   A render array representing the converter page content.
+   */
+  public function calendarPages() {
+    ob_start();
+
+    $month_names = [
+      1 => 'Phoenix',
+      'Cetus',
+      'Dorado',
+      'Lepus',
+      'Columba',
+      'Monoceros',
+      'Volans',
+      'Lynx',
+      'Camelopardalis',
+      'Chameleon',
+      'Hydra',
+      'Corvus',
+      'Centaurus',
+      'Draco',
+      'Lupus',
+      'Apus',
+      'Pavo',
+      'Aquila',
+      'Vulpecula',
+      'Cygnus',
+      'Delphinus',
+      'Grus',
+      'Pegasus',
+      'Tucana',
+    ];
+
+    $day_names = [
+      1 => 'Phobosol',
+      'Earthsol',
+      'Mercurisol',
+      'Jupitersol',
+      'Venusol',
+      'Deimosol',
+      'Sunsol',
+    ];
+
+    $seasons = ['spring', 'summer', 'autumn', 'winter'];
+
+    foreach ($month_names as $m => $month) {
+      // Get the season class.
+      $season = $seasons[floor(($m - 1) / 6)];
+
+      echo "<table class='calPage $season'>\n";
+      echo "<thead>\n";
+      echo "  <tr>\n";
+      echo "    <th class='monthName' colspan='7'>$m. $month</th>\n";
+      echo "  </tr>\n";
+      echo "</thead>\n";
+      echo "<tbody>\n";
+      echo "  <tr>\n";
+      echo "    <td class='monthIcon' colspan='7'><img src='/sites/default/files/month-icons/$month.png'></td>\n";
+      echo "  </tr>\n";
+
+      // Days of the week header.
+      echo "  <tr class='daysOfWeek squares'>\n";
+      for ($dow = 1; $dow <= 7; $dow++) {
+        echo "    <th>" . $day_names[$dow][0] . "</th>\n";
+      }
+      echo "  </tr>\n";
+
+      // Date squares.
+      for ($w = 1; $w <= 4; $w++) {
+        echo "  <tr class='squares'>\n";
+        for ($dow = 1; $dow <= 7; $dow++) {
+          $classes = [];
+
+          $sol = ($w - 1) * 7 + $dow;
+          $td = $sol;
+
+          // Add classes for special sols.
+          if ($m == 1 && $sol == 1) {
+            $classes[] = 'special-sol';
+            $classes[] = 'vernal-equinox';
+          }
+          elseif ($m == 6 && $sol == 12) {
+            $classes[] = 'special-sol';
+            $classes[] = 'aphelion';
+          }
+          elseif ($m == 6 && $sol == 28) {
+            $classes[] = 'empty';
+            $td = '&nbsp;';
+          }
+          elseif ($m == 7 && $sol == 27) {
+            $classes[] = 'special-sol';
+            $classes[] = 'summer-solstice';
+          }
+          elseif ($m == 12 && $sol == 28) {
+            $classes[] = 'empty';
+            $td = '&nbsp;';
+          }
+          elseif ($m == 14 && $sol == 11) {
+            $classes[] = 'special-sol';
+            $classes[] = 'autumnal-equinox';
+          }
+          elseif ($m == 18 && $sol == 12) {
+            $classes[] = 'special-sol';
+            $classes[] = 'perihelion';
+          }
+          elseif ($m == 18 && $sol == 28) {
+            $classes[] = 'empty';
+            $td = '&nbsp;';
+          }
+          elseif ($m == 19 && $sol == 14) {
+            $classes[] = 'special-sol';
+            $classes[] = 'winter-solstice';
+          }
+          elseif ($m == 24 && $sol == 28) {
+            $classes[] = 'special-sol';
+            $classes[] = 'intercalary';
+          }
+
+          echo "    <td";
+          if ($classes) {
+            echo ' class="' . implode(' ', $classes) . '"';
+          }
+          echo ">$td</td>\n";
+        }
+
+        echo "  </tr>\n";
+      }
+
+      echo "</tbody>\n";
+      echo "</table>\n";
+    }
+    $calendar_pages = ob_get_clean();
+
+    $build = [
+      '#type'      => 'markup',
+      '#theme'     => 'utopian_calendar_pages',
+      '#variables' => [
+        'calendar_pages' => $calendar_pages,
+      ],
+      '#cache'     => [
+        'max-age' => 0,
+      ],
+    ];
+    return $build;
+  }
 }
